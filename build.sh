5666341530f568fd7d7b2a73104085d34a00f29c
Jupiops <github@jupiops.net>
2021-09-03 20:35:56 +0200

    added github actions :construction_worker:

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-ndk-build NDK_PROJECT_PATH=x32 APP_BUILD_SCRIPT=x32/jni/Android.mk NDK_APPLICATION_MK=x32/jni/Application.mk
 ndk-build NDK_PROJECT_PATH=x64 APP_BUILD_SCRIPT=x64/jni/Android.mk NDK_APPLICATION_MK=x64/jni/Application.mk
+ndk-build NDK_PROJECT_PATH=x32 APP_BUILD_SCRIPT=x32/jni/Android.mk NDK_APPLICATION_MK=x32/jni/Application.mk
